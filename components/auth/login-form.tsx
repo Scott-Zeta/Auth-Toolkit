35843e69dc8ef3cc5eb8536eb59c3e763d1b7249
Scott Zeta <wc070211@gmail.com>
2024-01-04 15:55:22 +1030

    inital schema and login form hook

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,30 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { LoginSchema } from '@/schemas';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+
 import { CardWrapper } from './card-wrapper';
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="Welcome Back"
