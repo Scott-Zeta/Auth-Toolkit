98ee78a1977b6d7edf598357728ebe41beabe42d
Scott Zeta <wc070211@gmail.com>
2024-01-29 09:17:46 +1030

    Initial Prisma client

---
+++ lib/db.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+// use a global variable so the prisma client is only ever instantiated once
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = db;
+}
