bf5b479b0998593a4891d1c62ebbfd11c337a5a7
Scott Zeta <wc070211@gmail.com>
2024-01-30 09:56:21 +1030

    validate existing email

--- actions/register.ts
+++ actions/register.ts
@@ -2,21 +2,34 @@
 'use server';
 import * as z from 'zod';
 import bcrypt from 'bcrypt';
+import { db } from '@/lib/db';
 
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   //validation on server side
   const validateFields = RegisterSchema.safeParse(values);
 
-  if (validateFields.success) {
-    console.log(validateFields.data);
-    const { email, password, name } = validateFields.data;
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    return { success: 'Server validation success' };
+  if (!validateFields.success) {
+    return { error: 'Server validation failed' };
   }
 
-  return { error: 'Server validation failed' };
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: 'Email already in use.' };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  return { success: 'User created!' };
 };
