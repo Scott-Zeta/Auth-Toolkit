a2efdc9c7038417e2a630a27e7974493619fc2a4
Scott Zeta <wc070211@gmail.com>
2024-01-30 19:57:13 +1030

    Add authentication middleware logic to handle route access

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
 import authConfig from './auth.config';
 import NextAuth from 'next-auth';
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+  apiAuthPrefix,
+  authRoutes,
+} from './routes';
 
 const { auth } = NextAuth(authConfig);
 
@@ -9,6 +15,33 @@ export default auth((req) => {
   const isLoggedIn = !!req.auth;
   console.log('Route:', req.nextUrl.pathname);
   console.log('Is authenticated:', isLoggedIn);
+
+  const { nextUrl } = req;
+  const isAuthApiRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isAuthApiRoute) {
+    //always allow auth api routes
+    return null;
+  }
+
+  if (isAuthRoute) {
+    // allow auth routes if not logged in
+    // if already logged in, redirect to dashboard
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    // redirect any non-public routes without logged in
+    return Response.redirect(new URL('/auth/login', nextUrl));
+  }
+
+  // allow public routes by default
+  return null;
 });
 
 // Optionally, don't invoke Middleware on some paths
