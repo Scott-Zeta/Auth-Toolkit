dc93a0460b321f391fa21112f6ec06ac95a6b47c
Scott Zeta <wc070211@gmail.com>
2024-01-30 11:22:09 +1030

    set up auth middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { auth } from './auth';
+
+export default auth((req) => {
+  // Use the route and auth status to decide which is public or private route here
+  // Entire app is protected by default
+  const isLoggedIn = !!req.auth;
+  console.log('Route:', req.nextUrl.pathname);
+  console.log('Is authenticated:', isLoggedIn);
+});
+
+// Optionally, don't invoke Middleware on some paths
+// Everything match the regex will be invoke in the auth() middleware above
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
