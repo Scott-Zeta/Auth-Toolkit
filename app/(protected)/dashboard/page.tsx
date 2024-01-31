b816e2b621a186b754532bb83cb0499c827b1a23
Scott Zeta <wc070211@gmail.com>
2024-01-31 12:48:33 +1030

    add sign out event

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,9 +1,20 @@
-import { auth } from '@/auth';
+import { auth, signOut } from '@/auth';
+import { Button } from '@/components/ui/button';
 
 const dashboard = async () => {
   const session = await auth();
   return (
-    <div>Welcome to dash board. Your session is: {JSON.stringify(session)}</div>
+    <div>
+      Welcome to dash board. Your session is: {JSON.stringify(session)}
+      <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+      >
+        <Button type="submit">Sign Out</Button>
+      </form>
+    </div>
   );
 };
 
