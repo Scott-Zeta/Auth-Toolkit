e6af024c2cd3926ce4ff736ba7d364b8d44fdefc
Scott Zeta <wc070211@gmail.com>
2024-02-04 16:26:13 +1030

    verifycation page

---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import { BeatLoader } from 'react-spinners';
+import { useSearchParams } from 'next/navigation';
+
+import { CardWrapper } from '@/components/auth/card-wrapper';
+import { FormError } from '@/components/form-error';
+import { FormSuccess } from '@/components/form-success';
+
+export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get('token');
+
+  const onSubmit = useCallback(() => {
+    if (!token) {
+      setError('Missing token!');
+      return;
+    }
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center w-full justify-center">
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success} />
+        {!success && <FormError message={error} />}
+      </div>
+    </CardWrapper>
+  );
+};
