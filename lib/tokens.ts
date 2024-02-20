42ff5783f0a6fbcfa5af5e839a894c99be4ce4f7
Scott Zeta <wc070211@gmail.com>
2024-02-20 13:54:13 +1030

    two factor token generator

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -6,6 +6,31 @@ import { db } from '@/lib/db';
 import { getPasswordResetTokenByEmail } from '@/data/passwordRestToken';
 import { getTwoFactorTokenByEmail } from '@/data/twoFactorToken';
 
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
+};
+
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
   //token is going to expire in 30 minutes
