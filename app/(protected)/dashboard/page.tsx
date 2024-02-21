f85235abedb95b1262eb877c17f8b1f38bd8e74a
Scott Zeta <wc070211@gmail.com>
2024-02-21 18:30:19 +1030

    user button and nav bar for demo purpose

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,21 +1,5 @@
-import { auth, signOut } from '@/auth';
-import { Button } from '@/components/ui/button';
-
 const dashboard = async () => {
-  const session = await auth();
-  return (
-    <div>
-      Welcome to dash board. Your session is: {JSON.stringify(session)}
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
-        <Button type="submit">Sign Out</Button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default dashboard;
