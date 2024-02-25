1220be0f4e58d290af1c877676584a5a2efc4440
Scott Zeta <wc070211@gmail.com>
2024-02-25 17:14:18 +1030

    Role gate allowed multiple roles

--- components/auth/role-gate.tsx
+++ components/auth/role-gate.tsx
@@ -7,13 +7,13 @@ import { FormError } from '@/components/form-error';
 
 interface RoleGateProps {
   children: React.ReactNode;
-  allowedRole: UserRole;
+  allowedRole: UserRole[];
 }
 
 export const RoleGate = ({ children, allowedRole }: RoleGateProps) => {
   const role = useCurrentRole();
 
-  if (role !== allowedRole) {
+  if (!role || !allowedRole.includes(role)) {
     return (
       <FormError message="You do not have permission to view this content!" />
     );
